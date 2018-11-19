edd7ffaa2ed7cbf42c73479ea9cf387d8a6fd0c9
John F Morton <john@johnfmorton.com>
2018-11-19 15:00:45 -0500

    deploy

---
+++ js/about.c410466b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},o=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")]),n("p",[t._v("You're going to make a graph!")])])}],u=n("2877"),s={},i=Object(u["a"])(s,a,o,!1,null,null,null);i.options.__file="About.vue";e["default"]=i.exports}}]);
+//# sourceMappingURL=about.c410466b.js.map
